8adfa64ad9c0cb9d475f253c342edd6d3bbebe47
Grzesiek <grzesiek_103@o2.pl>
2019-04-28 22:50:54 +0200

    Add files to repo

---
+++ src/test/java/pl/edu/uph/tpsi/TpsiApplicationTests.java
@@ -0,0 +1,18 @@
+package pl.edu.uph.tpsi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith (SpringRunner.class)
+@SpringBootTest
+public class TpsiApplicationTests
+{
+
+        @Test
+        public void contextLoads ()
+        {
+        }
+
+}
