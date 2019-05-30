5ddcc79f565e08cb71a86f755761525f44d794af
Grzesiek <grzesiek_103@o2.pl>
2019-05-30 20:36:32 +0200

    Add image preview to disc add form

--- angularclient/src/app/shop/disc-form/disc-form.component.ts
+++ angularclient/src/app/shop/disc-form/disc-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DiscService } from '../../service/disc/disc.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Disc } from '../../model/disc';
+import { CategoryService } from '../../service/category/category.service';
+import { Category } from '../../model/category';
 
 @Component({
   selector: 'app-disc-form',
@@ -11,19 +13,36 @@ import { Disc } from '../../model/disc';
 export class DiscFormComponent implements OnInit {
 
   disc: Disc;
+  categories: Category[];
 
-  constructor(private route: ActivatedRoute, private router: Router, private discService: DiscService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private discService: DiscService,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit() {
     this.disc = new Disc();
+    this.disc.category = new Category();
     this.disc.images = [];
+    this.fetchCategories();
   }
 
   onSubmit() {
-    this.discService.save(this.disc).subscribe(result => this.goToDiscList(), res => alert('Failed to add new disc'));
+    this.discService.save(this.disc)
+      .subscribe(result => this.goToDiscList(), res => alert('Failed to add new disc'));
   }
 
   goToDiscList() {
     this.router.navigate(['/discs']);
+  }
+
+  fetchCategories() {
+    this.categoryService.findAll().subscribe(res => this.categories = res);
+  }
+
+  setCategory(id: any): void {
+    alert(id);
   }
 }
